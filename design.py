22e00fe5b1942456bbba749eed9a789aa7e510e3
Alexander Graf <mail@agraf.me>
2016-05-03 21:41:43 +0200

    Support most common stack-mounted LF engines

--- design.py
+++ design.py
@@ -4,16 +4,13 @@ from scipy.optimize import newton
 import parts
 
 class Design:
-    # mass, cost, min_acceleration
-    # engine
-    # fuel
-    #
     def printinfo(self):
         print("mass: %i kg" % self.mass)
         print("cost: %i" % self.cost)
         print("min_acceleration: %.1f m/s^2" % self.min_acceleration)
         print("engine: %s" % self.eng.name)
         print("fuel: %i kg (tank weight)" % self.fuel)
+        print("%s" % self.size)
         
 def lf_needed_fuel(delta_v, I_sp, m_p):
     def f(m_f):
@@ -25,16 +22,21 @@ def TryLFEngine(payload, pressure, dv, eng):
     design.mass = payload + eng.m
     design.cost = eng.cost
     design.eng = eng
+    design.size = eng.size
     isp = pressure*eng.isp_atm + (1-pressure)*eng.isp_vac
     F = pressure*eng.F_atm + (1-pressure)*eng.F_vac
     lf = lf_needed_fuel(dv, isp, design.mass)
     lf = lf * 9/8
-    # TODO: Small!?
-    smalltankcount = ceil(lf / parts.RocketFuelTanks[parts.Small_SmallestTank].m_full)
-    design.fuel = smalltankcount * parts.RocketFuelTanks[parts.Small_SmallestTank].m_full
+    # we only consider tanks with same radial size as engine.
+    smalltankcount = ceil(lf / parts.RocketFuelTanks[parts.SmallestTank[eng.size]].m_full)
+    design.fuel = smalltankcount * parts.RocketFuelTanks[parts.SmallestTank[eng.size]].m_full
     design.mass = design.mass + design.fuel
-    for i in range(parts.Small_SmallestTank, parts.Small_BiggestTank+1):
-        if i != parts.Small_BiggestTank:
+    # Fuel tank calculation:
+    # We use that
+    # - Tanks get always two times bigger
+    # - Bigger tanks are never more expensive
+    for i in range(parts.SmallestTank[eng.size], parts.BiggestTank[eng.size]+1):
+        if i != parts.BiggestTank[eng.size]:
             if smalltankcount % 2 != 0:
                 design.cost = design.cost + parts.RocketFuelTanks[i].cost
             smalltankcount = smalltankcount // 2
@@ -55,7 +57,8 @@ def FindDesigns(payload, pressure, dv, min_acceleration):
         if d.min_acceleration >= min_acceleration:
             designs.append(d)
 
-    # TODO: remove shit
+    # TODO: eliminate bad solutions
+
     # TODO: sort
 
     return designs
--- parts.py
+++ parts.py
@@ -8,15 +8,32 @@ class RadialSize(Enum):
     ExtraLarge = 4
     RadialMounted = 9
 
-# TODO: Add adapters, solid fuel boosters, radial engines, other engine types, etc.
+# TODO: Add adapters, solid fuel boosters, etc.
 
 LiquidFuelEngine = namedtuple('LiquidFuelEngine', ['size', 'name', 'cost', 'm', 'isp_atm', 'isp_vac', 'F_atm', 'F_vac'])
 # TODO: add more parameters to optimize for
 
-# TODO: fill table
+# TODO: support radially mounted engines
+# TODO: support monopropellant engines
+# TODO: support ion engines
+# TODO: support atomic engines
+# TODO: support engines which have tank included
+
 LiquidFuelEngines = [
+        LiquidFuelEngine(RadialSize.Tiny,  'LV-1 Ant',       110,  20,   80,  315, 510,    2000),
+        LiquidFuelEngine(RadialSize.Tiny,  '48-7S Spark',    200,  100,  270, 300, 16200,  18000),
         LiquidFuelEngine(RadialSize.Small, 'LV-909 Terrier', 390,  500,  85,  345, 14783,  60000),
-        LiquidFuelEngine(RadialSize.Small, 'LV-T30 Reliant', 1100, 1250, 280, 300, 200670, 215000) ]
+        LiquidFuelEngine(RadialSize.Small, 'LV-T30 Reliant', 1100, 1250, 280, 300, 200670, 215000),
+        LiquidFuelEngine(RadialSize.Small, 'LV-T45 Swivel',  1200, 1500, 270, 320, 168750, 200000),
+        LiquidFuelEngine(RadialSize.Small, 'S3 KS-25 Vector',18000,4000, 295, 315, 936500, 1000000),
+        LiquidFuelEngine(RadialSize.Small, 'CR7 RAPIER',     6000, 2000, 275, 305, 162300, 180000),
+        LiquidFuelEngine(RadialSize.Small, 'T-1 Dart',       3850, 1000, 290, 340, 153530, 180000),
+        LiquidFuelEngine(RadialSize.Large, 'RE-L10 Poodle',  1300, 1750, 90,  350, 64290,  250000),
+        LiquidFuelEngine(RadialSize.Large, 'RE-I5 Skipper',  5300, 3000, 280, 320, 568750, 650000),
+        LiquidFuelEngine(RadialSize.Large, 'RE-M3 Mainsail', 13000,6000, 285, 320, 1379000,1500000),
+        LiquidFuelEngine(RadialSize.ExtraLarge, 'KR-2L+ Rhino', 25000,9000, 255, 340, 1500000,2000000),
+        LiquidFuelEngine(RadialSize.ExtraLarge, 'KS-25x4 Mammoth', 39000,15000, 295, 315, 3746000,4000000) ]
+
 
 # liquid fuel tank quantities.
 # empty weight is 1/9 of full weight.
@@ -24,12 +41,20 @@ LiquidFuelEngines = [
 
 RocketFuelTank = namedtuple('RocketFuelTank', ['size', 'cost', 'm_full'])
 
-# TODO: fill table
 RocketFuelTanks = [
-        RocketFuelTank(RadialSize.Small, 150, 562.5),
+        RocketFuelTank(RadialSize.Tiny, 70, 225),               # 0
+        RocketFuelTank(RadialSize.Small, 150, 562.5),           # 1
         RocketFuelTank(RadialSize.Small, 275, 1125),
         RocketFuelTank(RadialSize.Small, 500, 2250),
-        RocketFuelTank(RadialSize.Small, 800, 4500) ]
+        RocketFuelTank(RadialSize.Small, 800, 4500),            # 4
+        RocketFuelTank(RadialSize.Large, 800, 4500),            # 5
+        RocketFuelTank(RadialSize.Large, 1550, 9000),
+        RocketFuelTank(RadialSize.Large, 3000, 18000),
+        RocketFuelTank(RadialSize.Large, 5750, 36000),          # 8
+        RocketFuelTank(RadialSize.ExtraLarge, 3250, 20250),     # 9
+        RocketFuelTank(RadialSize.ExtraLarge, 6500, 40500),
+        RocketFuelTank(RadialSize.ExtraLarge, 13000, 81000) ]   # 11
 
-Small_SmallestTank = 0
-Small_BiggestTank = 3
+SmallestTank = { RadialSize.Tiny : 0, RadialSize.Small: 1, RadialSize.Large: 5, RadialSize.ExtraLarge: 9 }
+BiggestTank =  { RadialSize.Tiny : 0, RadialSize.Small: 4, RadialSize.Large: 8, RadialSize.ExtraLarge: 11 }
+
