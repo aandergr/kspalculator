84bd4ae78f311b80d321013fbb62caeda1bfe04a
Alexander Graf <mail@agraf.me>
2016-05-13 21:04:12 +0200

    remove code redundancy

--- design.py
+++ design.py
@@ -4,20 +4,46 @@ from scipy.optimize import fsolve
 import parts
 
 class Design:
-    # TODO: invent better data structure for this thing
+    def __init__(self, payload, mainengine, mainenginecount, size):
+        self.mass = payload + mainenginecount*mainengine.m
+        self.cost = mainenginecount * mainengine.cost
+        self.mainengine = mainengine
+        self.mainenginecount = mainenginecount
+        self.size = size
+        self.min_acceleration = 0
+        self.fuel = 0
+    def AddLiquidFuelTanks(self, lf):
+        """lf should be full tank weight"""
+        smalltankcount = ceil(lf / parts.RocketFuelTanks[parts.SmallestTank[self.size]].m_full)
+        self.fuel = smalltankcount * parts.RocketFuelTanks[parts.SmallestTank[self.size]].m_full
+        self.mass = self.mass + self.fuel
+        # Fuel tank calculation:
+        # We use that
+        # - Tank size is 2^n times the size of smallest tank with that radius
+        # - It is cheapest to use the biggest tank possible
+        for i in range(parts.SmallestTank[self.size], parts.BiggestTank[self.size]+1):
+            if i != parts.BiggestTank[self.size]:
+                if smalltankcount % 2 != 0:
+                    self.cost = self.cost + parts.RocketFuelTanks[i].cost
+                smalltankcount = smalltankcount // 2
+            else:
+                self.cost = self.cost + smalltankcount * parts.RocketFuelTanks[i].cost
+    def CalculateMinAcceleration(self, pressure):
+        F = self.mainenginecount * engine_force(self.mainengine, pressure)
+        self.min_acceleration = F / self.mass
     def printinfo(self):
-        if self.engcount == 1:
-            print("%s" % self.eng.name)
+        if self.mainenginecount == 1:
+            print("%s" % self.mainengine.name)
         else:
-            print("%i * %s, radially mounted" % (self.engcount, self.eng.name))
+            print("%i * %s, radially mounted" % (self.mainenginecount, self.mainengine.name))
         print("\ttotal mass: %i kg (including payload)" % self.mass)
         print("\tcost: %i" % self.cost)
         print("\tmin_acceleration: %.1f m/s^2" % self.min_acceleration)
         # TODO: print delta v
         print("\tfuel: %i units (%i kg full tank weight)" % (self.fuel*8/9*0.2, self.fuel))
-        print("\tgimbal: %.1f °" % self.eng.tvc)
+        print("\tgimbal: %.1f °" % self.mainengine.tvc)
         print("\t%s" % self.size)
-        print("\t%s" % self.eng.level)
+        print("\t%s" % self.mainengine.level)
     def IsBetterThan(self, a, preferredsize, bestgimbal):
         """
         Returns True if self is better than a by any parameter, i.e. there might
@@ -30,21 +56,22 @@ class Design:
         # the minimum required acceleration is usually not useful
         # check if we have better gimbal
         if bestgimbal:
-            if self.eng.tvc > a.eng.tvc:
+            if self.mainengine.tvc > a.mainengine.tvc:
                 return True
         else:
-            if self.eng.tvc > 0.0 and a.eng.tvc == 0.0:
+            if self.mainengine.tvc > 0.0 and a.mainengine.tvc == 0.0:
                 return True
         # this is where user's size preferrence comes in
         if preferredsize is not None:
             if self.size is preferredsize and a.size is not preferredsize:
                 return True
         # check if self uses simpler technology
-        if a.eng.level is not self.eng.level and a.eng.level.DependsOn(self.eng.level):
+        if a.mainengine.level is not self.mainengine.level and a.mainengine.level.DependsOn(self.mainengine.level):
             return True
         return False
 
 def lf_needed_fuel(dv, I_sp, m_p):
+    """Returns required fuel weight"""
     g_0 = 9.81
     f_e = 1/8   # empty weight fraction
     def equations(m):
@@ -55,63 +82,25 @@ def lf_needed_fuel(dv, I_sp, m_p):
     sol = fsolve(equations, [0 for i in range(len(I_sp))])
     return sol[0]
 
-# TODO: unify design creation
+def engine_isp(eng, pressure):
+    return [pressure[i]*eng.isp_atm + (1-pressure[i])*eng.isp_vac for i in range(len(pressure))]
+
+def engine_force(eng, pressure):
+    return pressure[0]*eng.F_atm + (1-pressure[0])*eng.F_vac
+
+# TODO: simplify design creation even more
 
 def CreateSingleLFEngineDesign(payload, pressure, dv, eng):
-    design = Design()
-    design.mass = payload + eng.m
-    design.cost = eng.cost
-    design.eng = eng
-    design.engcount = 1
-    design.size = eng.size
-    isp = [pressure[i]*eng.isp_atm + (1-pressure[i])*eng.isp_vac for i in range(len(pressure))]
-    F = pressure[0]*eng.F_atm + (1-pressure[0])*eng.F_vac
-    lf = lf_needed_fuel(dv, isp, design.mass)
-    lf = lf * 9/8
-    # we only consider tanks with same radial size as engine.
-    smalltankcount = ceil(lf / parts.RocketFuelTanks[parts.SmallestTank[eng.size]].m_full)
-    design.fuel = smalltankcount * parts.RocketFuelTanks[parts.SmallestTank[eng.size]].m_full
-    design.mass = design.mass + design.fuel
-    # Fuel tank calculation:
-    # We use that
-    # - Tanks get always two times bigger
-    # - Bigger tanks are never more expensive
-    for i in range(parts.SmallestTank[eng.size], parts.BiggestTank[eng.size]+1):
-        if i != parts.BiggestTank[eng.size]:
-            if smalltankcount % 2 != 0:
-                design.cost = design.cost + parts.RocketFuelTanks[i].cost
-            smalltankcount = smalltankcount // 2
-        else:
-            design.cost = design.cost + smalltankcount * parts.RocketFuelTanks[i].cost
-    design.min_acceleration = F / design.mass
+    # TODO: have more options regarding radial size
+    design = Design(payload, eng, 1, eng.size)
+    design.AddLiquidFuelTanks(9/8 * lf_needed_fuel(dv, engine_isp(eng, pressure), design.mass))
+    design.CalculateMinAcceleration(pressure)
     return design
 
 def CreateRadialLFEnginesDesign(payload, pressure, dv, eng, size, count):
-    design = Design()
-    design.mass = payload + count*eng.m
-    design.cost = count*eng.cost
-    design.eng = eng
-    design.engcount = count
-    design.size = size
-    isp = [pressure[i]*eng.isp_atm + (1-pressure[i])*eng.isp_vac for i in range(len(pressure))]
-    F = count * (pressure[0]*eng.F_atm + (1-pressure[0])*eng.F_vac)
-    lf = lf_needed_fuel(dv, isp, design.mass)
-    lf = lf * 9/8
-    smalltankcount = ceil(lf / parts.RocketFuelTanks[parts.SmallestTank[size]].m_full)
-    design.fuel = smalltankcount * parts.RocketFuelTanks[parts.SmallestTank[size]].m_full
-    design.mass = design.mass + design.fuel
-    # Fuel tank calculation:
-    # We use that
-    # - Tanks get always two times bigger
-    # - Bigger tanks are never more expensive
-    for i in range(parts.SmallestTank[size], parts.BiggestTank[size]+1):
-        if i != parts.BiggestTank[size]:
-            if smalltankcount % 2 != 0:
-                design.cost = design.cost + parts.RocketFuelTanks[i].cost
-            smalltankcount = smalltankcount // 2
-        else:
-            design.cost = design.cost + smalltankcount * parts.RocketFuelTanks[i].cost
-    design.min_acceleration = F / design.mass
+    design = Design(payload, eng, count, size)
+    design.AddLiquidFuelTanks(9/8 * lf_needed_fuel(dv, engine_isp(eng, pressure), design.mass))
+    design.CalculateMinAcceleration(pressure)
     return design
 
 # TODO: add ship designs using atomic rocket motor, monopropellant engine, ion engine
@@ -120,8 +109,6 @@ def CreateRadialLFEnginesDesign(payload, pressure, dv, eng, size, count):
 # TODO: add asparagous designs
 
 # TODO: consider ship width and adapters
-
-# TODO: support delta-v with different pressure
 
 def FindDesigns(payload, pressure, dv, min_acceleration, preferredsize = None, bestgimbal = False):
     """
