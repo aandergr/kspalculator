28061c3b0ac2d4193e038b295557c175994619ad
Alexander Graf <mail@agraf.me>
2016-05-22 15:07:05 +0200

    add more sflf_needed_fuel tests

--- unittests.py
+++ unittests.py
@@ -26,10 +26,20 @@ class TestPhysics(TestCase):
         self.assertListAlmostEqual(r_a_t, [13.19, 15.65, 17.15, 21.68, 22.81])
         self.assertListAlmostEqual(r_m_s, [7630.0, 4548.69, 3832.08, 3496.57, 2766.80])
         self.assertListAlmostEqual(r_m_t, [4548.69, 3832.08, 3496.57, 2766.80, 2630.0])
-    # TODO: have a very deep look to test_sflf_needed_fuel and test_sflf_performance correctness
     def test_sflf_needed_fuel(self):
         m_c = physics.sflf_needed_fuel([2500, 2000], [250, 320], [195,220], 15000, 50, 24000, 4500)
         self.assertAlmostEqual(m_c, 104716.64, places=1)
+        # tests which once failed
+        m_c = physics.sflf_needed_fuel([2000], [250], [150], 10000, 200, 10000, 2000)
+        self.assertAlmostEqual(m_c, 10106.61, places=1)
+        m_c = physics.sflf_needed_fuel([150, 2000], [240, 250], [130, 150], 10000, 200, 10000, 2000)
+        self.assertAlmostEqual(m_c, 12298.56, places=1)
+        m_ca = physics.sflf_needed_fuel([2150], [250], [150], 10000, 200, 10000, 2000)
+        m_cb = physics.sflf_needed_fuel([150, 2000], 2*[250], 2*[150], 10000, 200, 10000, 2000)
+        self.assertAlmostEqual(m_ca, 11990.20, places=1)
+        self.assertAlmostEqual(m_cb, 11990.20, places=1)
+        m_c = physics.sflf_needed_fuel([905, 3650], [260, 284.6], [195, 215.5], 10040, 50, 24000, 4500)
+        self.assertAlmostEqual(m_c, 63162.60, places=1)
     def test_sflf_performance(self):
         r_dv, r_p, r_a_s, r_a_t, r_m_s, r_m_t, r_solid, r_op = \
                 physics.sflf_performance([1000, 500], [250, 260], [150, 170],
