94d31d822f7ba8917e9dc753d2630db24eb966ae
T. Alexander Popiel <tapopiel@gmail.com>
2017-10-27 12:43:12 -0700

    Update unit test for extra Finder argument

--- test/test_finder.py
+++ test/test_finder.py
@@ -8,7 +8,7 @@ class TestFinder(unittest.TestCase):
     def test_finder(self):
         """ check whether integration of kspalculator as a module works """
         f = Finder(1320, RadialSize.Small,
-                [1170, 580, 580, 210, 700], [0.0, 3.3, 5.0, 0.0, 0.0], 5*[0.0],
+                [1170, 580, 580, 210, 700], [0.0, 3.3, 5.0, 0.0, 0.0], 5*[0.0], 5*[True],
                 False, False, False, True, True)
         designs = f.find()
         self.assertEqual(len(designs), 7)
