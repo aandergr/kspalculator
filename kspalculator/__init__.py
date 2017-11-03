b9fbe946f297b56ddf681c59480212a522c745b4
Alexander Graf <mail@agraf.me>
2017-11-03 11:11:07 +0100

    Version 0.11

--- kspalculator/__init__.py
+++ kspalculator/__init__.py
@@ -1,4 +1,4 @@
 """Determine the best rocket propulsion designs for one stage of a rocket, given a set of
 constraints and preferences (Kerbal Space Program)."""
 
-__version__ = '0.10.2'
+__version__ = '0.11'
